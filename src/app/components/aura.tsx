e754f66293b11e944e722db12bef4b69be4047ba
yonaries <yonatandejene001@gmail.com>
2025-09-06 13:33:07 +0300

    Initial commit

---
+++ src/app/components/aura.tsx
@@ -0,0 +1,8 @@
+"use client"
+import UnicornScene from "unicornstudio-react";
+
+export default function Aura() {
+  return (
+    <UnicornScene className="opacity-70" projectId="p5ZNVmnVWReXFMARl6xI" width={1440} height={1024} />
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Aura from "./components/aura";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <main>
+        <Aura/>
+      </main>
+    </div>
+  );
+}
